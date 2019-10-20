7549ce7380f43560d8965e410e8ec744427d5ea2
vinigl <vinicius.gl098@gmail.com>
2019-10-20 20:39:11 -0300

    alter table script

--- api/api.py
+++ api/api.py
@@ -16,45 +16,28 @@ from models import *
 app = FastAPI()
 
 
-@app.get("/")
-def read_root(req : Request):
+@app.post("/")
+def read_root(teste: Teste):
     
-    #     raise HTTPException(status_code=404, detail="Erro na operacao")
-    infos = vars(req.headers)['_list']
-   
-    request_infos = {}
-    for i in infos:
-        key = i[0].decode('ASCII')
-        value = i[1].decode('ASCII')
-
-        request_infos[key] = value
-
-    OS = request_infos['user-agent'].split(" ")[2]
-    
-    useful_info = {'host': request_infos['host'], 
-                   'browser': request_infos['user-agent'], 
-                   'OS' : OS, 
-                   'criado_ts': datetime.datetime.now()}
-    
-    return useful_info
+    return 
 
 
 @app.get("/usuario")  # Procura usuario por nome e sobrenomes
-def acha_usuario(usuario: Usuario):
+def acha_usuario(usuario: Acha_Usuario):
     result = acha_usuario_nome(connection, usuario.nome, usuario.sobrenome)
     return result
 
 
 @app.post("/usuario")  # Insere usuario
-def cria_usuario(usuario: Usuario):
-    nome_cidade = usuario.cidade.capitalize()
-    id_cidade = get_nome_cidade(connection, nome_cidade)
+def cria_usuario(usuario: Adiciona_Usuario):
+    # nome_cidade = usuario.cidade.capitalize()
+    # id_cidade = get_nome_cidade(connection, nome_cidade)
 
     usuario_add = {
         'nome': usuario.nome,
         'sobrenome': usuario.sobrenome,
         'email': usuario.email,
-        'cidade_idcidade': id_cidade,
+        'cidade_idcidade': usuario.idcidade,
         'login': usuario.login
     }
 
@@ -98,6 +81,10 @@ def acha_posts(post: Post_Acha, req: Request):
 
     return resultado
 
+@app.get("/os_popular")
+def lista_os():
+    resultado = os_popular(connection)
+    return resultado
 
 @app.post("/posts")
 def cria_post(post: Post_Cria):
